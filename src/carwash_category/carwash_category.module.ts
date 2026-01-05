b3adeae1c819f4731bd4f79291dbb0a6644e1670
Thanakon Khuadreem <thanakon69798@gmail.com>
2026-01-05 16:13:44 +0700

    ลบไฟล์ที่ไม่ต้องการออก

--- src/carwash_category/carwash_category.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CarwashCategoryService } from './carwash_category.service';
-import { CarwashCategoryController } from './carwash_category.controller';
-
-@Module({
-  controllers: [CarwashCategoryController],
-  providers: [CarwashCategoryService],
-})
-export class CarwashCategoryModule {}
--- src/carwash_category/dto/update-carwash_category.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCarwashCategoryDto } from './create-carwash_category.dto';
-
-export class UpdateCarwashCategoryDto extends PartialType(CreateCarwashCategoryDto) {}
